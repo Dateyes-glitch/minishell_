0f982ed53610491fbd9777b7f1bb33557893e9a3
Sativabèbè <121039009+xoxowasgehtab@users.noreply.github.com>
2024-09-27 18:16:30 +0200

    env vars working

---
+++ mini:sativa:glitch:19:sep/mini:sativa:glitch:19:sep/env_var.c
@@ -0,0 +1,144 @@
+
+#include "minishell.h"
+
+void    display_env_vars(envvar **env_list)
+{
+    envvar *current;
+
+    current = *env_list;
+    while (current)
+    {
+        printf("%s%c%s\n", current->key, '=', current->value);
+        current = current->next;
+    }
+}
+
+void    add_to_list(envvar **env_list, char *key, char *value)
+{
+    envvar  *new_node;
+    envvar  *current;
+
+    new_node = malloc(sizeof(envvar));
+    if (!new_node)
+    {
+        perror("Failed to allocate memory for list");
+        return ;
+    }
+    new_node->key = strdup(key);
+    new_node->value = strdup(value);
+    new_node->next = NULL;
+    
+    if (*env_list == NULL)
+        *env_list = new_node;
+    else
+    {
+        current = *env_list;
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new_node;
+    }
+}
+
+void    initialize_env(envvar **env_list)
+{
+    extern char **environ;
+    char **env;
+    char *name;
+    char *value;
+    char    *split_pos;
+
+    env = environ;
+    while (*env)
+    {
+        split_pos = strchr(*env, '=');
+        if (split_pos != NULL)
+        {
+            *split_pos = '\0';
+            name = *env;
+            value = split_pos + 1;
+            add_to_list(env_list, name, value);
+        }
+        env++;
+    }
+}
+
+char *get_env_var(envvar **env_list, char *key)
+{
+    envvar *current;
+
+    current = *env_list;
+    while (current)
+    {
+        if (strcmp(current->key, key) == 0)
+            return (current->value);
+        current = current->next;
+    }
+    return (NULL);
+}
+
+void    unset_env_var(envvar **env_list, char *key)
+{
+    envvar *current;
+    envvar *previous;
+
+    current = *env_list;
+    previous = NULL;
+    while (current)
+    {
+        if (strcmp(current->key, key) == 0)
+        {
+            if (previous)
+                previous->next = current->next;
+            else
+                *env_list = current->next;
+            free(current->key);
+            free(current->value);
+            free(current);
+            return ;
+        }
+        previous = current;
+        current = current->next;
+    }
+}
+
+void    ft_free(envvar **env_list)
+{
+    envvar  *current;
+    envvar  *next_node;
+
+    current = *env_list;
+    while (current)
+    {
+        next_node = current->next;
+        free(current->key);
+        free(current->value);
+        free(current);
+        current = next_node;
+    }
+    *env_list = NULL;
+}
+
+// int main()
+// {
+//     envvar *env_list = NULL;
+//     char *var = "HELLO";
+
+//     initialize_env(&env_list);
+//     // print_list(env_list);
+//     add_or_update_var(&env_list, var, "123");
+//     // printf("\n\n\n");
+//     // print_list(env_list);
+//     add_or_update_var(&env_list, var, "123456");
+//     add_or_update_var(&env_list, "BLA", "bitch");
+//     add_or_update_var(&env_list, "YESSIR", "please");
+//     printf("\n\n\n");
+//     print_list(env_list);
+//     // display_export_vars(&env_list);
+//     // char *var2 = get_env_var(&env_list, "HELLO");
+//     // printf("\n\n\n");
+//     // printf("var : %s\n", var2);
+//     unset_env_var(&env_list, "BLA");
+//     printf("\n\n\n");
+//     print_list(env_list);
+//     return 0;
+// }
---
+++ mini:sativa:glitch:19:sep/mini:sativa:glitch:19:sep/minishell.h
@@ -0,0 +1,78 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include "env_var.h"
+#include "tokens.h"
+#include "cmds.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <string.h>
+#include <errno.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+
+#define NUM_BUILTINS 7
+#define BUF_SIZE 64
+
+typedef struct envvar 
+{
+    char *key;
+    char *value;
+    struct envvar *next;
+}   envvar;
+
+typedef struct builtin
+{
+  char *name;
+  int(*func)(Command *cmd, envvar **env_list);
+} builtin_cmd_t;
+
+
+void    display_env_vars(envvar **env_list);
+void    add_to_list(envvar **env_list, char *key, char *value);
+void    initialize_env(envvar **env_list);
+void    add_or_update_var(envvar **env_list, char *key, char *value);
+char    *get_env_var(envvar **env_list, char *key);
+void    display_export_vars(envvar **env_list);
+void    unset_env_var(envvar **env_list, char *key);
+void    ft_free(envvar **env_list);
+
+
+void ft_init_builtins(builtin_cmd_t *builtins);
+int ft_run_builtin(builtin_cmd_t *builtins, char **args);
+int cmd_cd(Command *cmd, envvar **env_list);
+int cmd_echo(Command *cmd, envvar **env_list);
+int cmd_pwd(Command *cmd, envvar **env_list);
+int cmd_export(Command *cmd, envvar **env_list);
+int cmd_unset(Command *cmd, envvar **env_list);
+int cmd_env(Command *cmd, envvar **env_list);
+int cmd_exit(Command *cmd, envvar **env_list);
+int ft_execute_builtin(Command *cmd, builtin_cmd_t *builtins, envvar **env_list);
+int ft_execute_external(char **args, Command *cmd);
+char *find_executable(char *command);
+char *ft_read_input(void);
+char **ft_split_input(char *input);
+int ft_execute_command(char **args, builtin_cmd_t *builtins);
+void ft_run_shell(void);
+char *ft_strdup(const char *s);
+void handle_redirections(Command *cmd);
+
+# define    RESET      "\033[0m"
+# define    ITALIC       "\033[3m"
+# define    BOLD_ITALIC_MAGENTA "\033[1;3;35m"
+# define    UNDERLINE  "\033[4m"
+# define    UNDERLINE_B_I_MAGENTA "\033[1;3;4;35m"
+# define    BOLD_GREEN "\033[1;32m"
+# define    BOLD_BLUE   "\033[1;34m"
+# define    BOLD_CYAN   "\033[1;36m"
+# define    ARROW       "➜ "
+
+#endif
